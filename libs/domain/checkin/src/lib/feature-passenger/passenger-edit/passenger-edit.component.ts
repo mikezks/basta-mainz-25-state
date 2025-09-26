2799cf216cb566e2584e95a130ca9957975d7843
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-26 15:39:18 +0200

    demo: signal forms

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,27 +1,32 @@
-import { Component, inject } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { validatePassengerStatus } from '../../util-validation';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Control, form, required, schema } from '@angular/forms/signals';
+import { initialPassenger, Passenger } from '../../logic-passenger';
 
+// (3) Field Logic: validate, readonly, disabled, hidden
+
+const passengerSchema = schema<Passenger>(passengerPath => {
+  required(passengerPath.name);
+  required(passengerPath.firstName);
+});
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // (4) UI Control: Template Binding
+    Control
   ],
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
-  protected editForm = inject(NonNullableFormBuilder).group({
-    id: [0],
-    firstName: [''],
-    name: [''],
-    bonusMiles: [0],
-    passengerStatus: ['', [
-      validatePassengerStatus(['A', 'B', 'C'])
-    ]]
-  });
+  // (1) Data Model: Writable Signal
+  protected passenger = signal(initialPassenger);
+
+  // (2) Field State: valid, touched, dirty, disabled
+  protected editForm = form(this.passenger, passengerSchema);
 
   protected save(): void {
-    console.log(this.editForm.value);
+    console.log(this.editForm().value());
   }
 }
